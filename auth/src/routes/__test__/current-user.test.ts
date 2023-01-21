17dd161b8de349b349dd5c80ee77274f0be8ecd6
Thanh Tung <nguyenththanhtung317@gmail.com>
2023-01-21 22:21:05 +0700

    add test case for current user

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,14 +1,23 @@
-import request from 'supertest'
+import request from 'supertest';
 import { app } from '../../app';
 
-it('response with detail about the current user', async ()=>{
-    const cookie = await global.signin()
+it('response with detail about the current user', async () => {
+  const cookie = await global.signin();
 
-    const resp = await request(app)
-      .get('/api/users/currentuser')
-      .set('Cookie', cookie)
-      .send()
-      .expect(200)
+  const resp = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
 
-    expect(resp.body.currentUser.email).toEqual('test@test.com')
-})
+  expect(resp.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+  const resp = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(resp.body.currentUser).toEqual(null);
+});

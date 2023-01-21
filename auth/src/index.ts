ea97d0f0370815d3a17c8477c217e86dddc34049
Thanh Tung <nguyenththanhtung317@gmail.com>
2023-01-21 16:42:34 +0700

    setup for test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,8 @@
-import express from 'express';
-import 'express-async-errors'
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session'
-
-import { errorHandler } from './middleware/error-handler';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { NotFoundError } from './errors/not-found-error';
+import {app} from './app'
 import { DatabaseConnectionError } from './errors/database-connection-error';
 
-const app = express();
-app.set('trust proxy', true)
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*',async (req, res, next)=>{
-    throw new NotFoundError()
-})
-
-app.use(errorHandler);
 
 const start =async () => {
   if (!process.env.JWT_KEY){

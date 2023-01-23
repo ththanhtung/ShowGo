5c503635ebc9fe5dfa109c7ce58bf00257e117c0
Thanh Tung <nguyenththanhtung317@gmail.com>
2023-01-24 05:17:00 +0700

    create MONGO_URI

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@vigacorp/common';
 
 const app = express();
 app.set('trust proxy', true);

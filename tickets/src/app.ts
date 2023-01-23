5dd237ea7c590714f76e223853f558c77c892c0e
Thanh Tung <nguyenththanhtung317@gmail.com>
2023-01-24 04:58:04 +0700

    create deployment service and skaffold for tickets service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,12 +2,6 @@ import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@vigacorp/common';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
 const app = express();
 app.set('trust proxy', true);
 
@@ -19,10 +13,6 @@ app.use(
   })
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
